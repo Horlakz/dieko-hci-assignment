dc0533325abbf122b943bf8a51a17f0e80932673
horlakz <officialhorlakz@gmail.com>
2024-03-26 11:19:37 +0100

    feat(src/routes/gallery/): updates images and image size in page

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,25 +1,37 @@
-<script>
+<script lang="ts">
+	import gallery1 from '$lib/assets/gallery-1.jpg';
+	import gallery10 from '$lib/assets/gallery-10.jpg';
+	import gallery11 from '$lib/assets/gallery-11.jpg';
 	import gallery2 from '$lib/assets/gallery-2.jpg';
 	import gallery3 from '$lib/assets/gallery-3.jpg';
 	import gallery4 from '$lib/assets/gallery-4.jpg';
 	import gallery5 from '$lib/assets/gallery-5.jpg';
-	import gallery from '$lib/assets/gallery.jpg';
+	import gallery6 from '$lib/assets/gallery-6.jpg';
+	import gallery7 from '$lib/assets/gallery-7.jpg';
+	import gallery8 from '$lib/assets/gallery-8.jpg';
+	import gallery9 from '$lib/assets/gallery-9.jpg';
 
-	const images = [gallery, gallery2, gallery3, gallery4, gallery5];
+	const images = [
+		gallery1,
+		gallery2,
+		gallery3,
+		gallery4,
+		gallery5,
+		gallery6,
+		gallery7,
+		gallery8,
+		gallery9,
+		gallery10,
+		gallery11
+	];
 </script>
 
-<section class="flex-col-center gap-6 sm:px-20 px-6">
+<section class="flex-col-center gap-4 sm:px-20 px-6">
 	<h2 class="sm:text-5xl text-4xl font-semibold">Gallery</h2>
 
-	<section class="grid sm:grid-cols-4 gap-6 px-6 py-8">
+	<section class="grid sm:grid-cols-3 sm:gap-12 gap-6 px-6 py-8">
 		{#each images as image, index}
-			<img
-				src={image}
-				alt="image {index}"
-				width="130"
-				height="130"
-				class="shadow-image rounded-lg"
-			/>
+			<img src={image} alt="image {index}" class="shadow-image w-48 h-40 rounded-lg" />
 		{/each}
 	</section>
 </section>
