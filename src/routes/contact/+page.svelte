2b0ddde279d3e36b95c7e03626c9c90145e02f2d
horlakz <officialhorlakz@gmail.com>
2024-03-26 10:34:18 +0100

    feat(/src/routes/contact/): updates contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,18 +1,41 @@
 <script>
+	import EducationIcon from '$lib/icons/EducationIcon.svelte';
 	import EnvelopeIcon from '$lib/icons/EnvelopeIcon.svelte';
-	import PhoneIcon from '$lib/icons/PhoneIcon.svelte';
+	import InstagramIcon from '$lib/icons/InstagramIcon.svelte';
+	import WhatsappIcon from '$lib/icons/WhatsappIcon.svelte';
+
+	const details = [
+		{
+			icon: EnvelopeIcon,
+			value: 'diekoloreoluwadavid@gmail.com'
+		},
+		{
+			icon: WhatsappIcon,
+			value: '+234 813 4909 239'
+		},
+		{
+			icon: InstagramIcon,
+			value: 'diekoloreoluuwa'
+		},
+		{
+			icon: EducationIcon,
+			value: 'EES/20/21/0202'
+		}
+	];
 </script>
 
 <div class="w-full h-full flex-center">
 	<section
-		class="shadow-text rounded-tr-3xl rounded-bl-3xl border border-black sm:p-6 p-4 sm:mx-16 mx-6 max-h-[40rem] overflow-scroll text-center"
+		class="shadow-text rounded-tr-3xl rounded-bl-3xl border border-black sm:p-6 p-4 sm:mx-16 mx-4 text-center"
 	>
 		<h1 class="uppercase sm:text-5xl text-2xl font-bold mb-6">contact me</h1>
 		<ul>
-			<li class="flex items-center gap-4">
-				<EnvelopeIcon color="#000" /><span>johndoe@gmail.com</span>
-			</li>
-			<li class="flex items-center gap-4"><PhoneIcon color="#000" /><span>+1 23 245 2312</span></li>
+			{#each details as item}
+				<li class="flex items-center gap-4">
+					<svelte:component this={item.icon} color={'#000'} />
+					<span>{item.value} </span>
+				</li>
+			{/each}
 		</ul>
 	</section>
 </div>
