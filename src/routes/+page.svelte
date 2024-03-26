830e124bf64c6c308190e34cd20dcc7a17f28038
horlakz <officialhorlakz@gmail.com>
2024-03-26 09:48:01 +0100

    feat(/src/routes/): updates text on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 	/>
 
 	<div class="uppercase flex-col-center sm:gap-8 gap-2">
-		<h3 class="font-bold sm:text-5xl text-xl">diekoloreloluwa idowu</h3>
+		<h3 class="font-bold sm:text-4xl text-lg">idowu david diekoloreloluwa</h3>
 		<p class="font-light text-gray-800 sm:text-4xl text-xl">EES/20/21/0202</p>
 		<div class="flex-center gap-6">
 			<Button on:click={() => goto('/about')}>about me</Button>
