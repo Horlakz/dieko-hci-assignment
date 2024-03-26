b1e7b5d54fefb179993ff574f56b2276a93aa695
horlakz <officialhorlakz@gmail.com>
2024-03-26 08:46:08 +0100

    feat(/src/routes/about-hci/): adds about hci page

---
+++ src/routes/about-hci/+page.svelte
@@ -0,0 +1,30 @@
+<section class="flex-col-center gap-6 px-20">
+	<h2 class="text-3xl font-bold">About Human Computer Interaction &#40;HCI&#41;</h2>
+
+	<section class="w-full flex flex-col justify-start items-start">
+		<h3 class="border-b-2 border-black w-fit text-lg font-bold pr-28">Meaning of HCI?</h3>
+		<p>
+			Human-computer interaction (HCI) is a field that studies the interaction between humans and
+			computers.
+			<br />
+			It focuses on designing and evaluating computer technology to ensure it's effective, efficient,
+			and user-friendly.
+			<br />
+			HCI draws on various disciplines like computer science, psychology, design, and ergonomics.
+		</p>
+	</section>
+
+	<section class="w-full flex flex-col justify-start items-start">
+		<h3 class="border-b-2 border-black w-fit text-lg font-bold pr-28">History is HCI?</h3>
+		<p>
+			The history of HCI goes back to the early days of computing when user interfaces were complex
+			and challenging to use.
+			<br />
+			As technology evolved from mainframes to personal computers, the need for more intuitive interfaces
+			grew.
+			<br />
+			HCI research played a key role in developing graphical user interfaces (GUIs), making computers
+			more accessible.
+		</p>
+	</section>
+</section>
